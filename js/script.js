3f60f056e8c9999c05193118e2ba688b9ef93bb2
Cix <gabrielviniciodacosta@gmail.com>
2023-08-09 09:53:14 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 let scriptElement = document.createElement('script');
-// Defina o atributo src para o URL do Bootstrap JavaScript
 scriptElement.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
-// Adicione o elemento script ao final do corpo do documento
 document.body.appendChild(scriptElement);
